43f236283111143927018d1d706c50ee0fa7e847
Santos Alarcón Asensio <santosalarcon86@gmail.com>
2024-07-29 14:55:07 +0200

    chore: add aliases to tsconfig

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,15 @@
-import i18nConfig from '@/i18n.config';
-import { i18nRouter } from 'next-i18n-router';
-import type { NextRequest } from 'next/server';
+import i18nConfig from "@/i18n.config";
+import { i18nRouter } from "next-i18n-router";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+	// Register the URL param in the headers
+	request.headers.set("x-current-path", request.nextUrl.pathname);
 
-    // Register the URL param in the headers
-    request.headers.set("x-current-path", request.nextUrl.pathname);
-
-    return i18nRouter(request, i18nConfig.i18n);
+	return i18nRouter(request, i18nConfig.i18n);
 }
 
 // applies this middleware only to files in the app directory
 export const config = {
-    matcher: '/((?!api|static|.*\\..*|_next).*)',
+	matcher: "/((?!api|static|.*\\..*|_next).*)",
 };
