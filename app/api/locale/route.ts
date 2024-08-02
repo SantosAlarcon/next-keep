7f424777cab349b7f35e3a3d3d92f2a35fd55f50
Santos Alarcón Asensio <santosalarcon86@gmail.com>
2024-08-02 21:24:42 +0200

    Update route.ts

--- app/api/locale/route.ts
+++ app/api/locale/route.ts
@@ -4,7 +4,10 @@ import type { NextRequest } from "next/server";
  * @swagger
  * /api/locale:
  *   get:
- *     description: Returns the current locale
+ *     tags:
+ *       - locale
+ *     summary: Returns the current locale
+ *     description: Returns the current locale from the 'NEXT_LOCALE' cookie.
  *     responses:
  *       200:
  *         description: Returns the current locale
