ded60df249d1a778a45be7c2d3dad7d391530cf0
Santos Alarcón Asensio <santosalarcon86@gmail.com>
2024-08-02 21:23:18 +0200

    Create react-swagger.tsx

---
+++ app/api/docs/react-swagger.tsx
@@ -0,0 +1,19 @@
+"use client";
+import dynamic from "next/dynamic";
+import "swagger-ui-react/swagger-ui.css";
+
+const DynamicSwaggerUI = dynamic(() => import("swagger-ui-react"), {
+	ssr: false,
+	loading: () => <p>Loading Component...</p>,
+});
+
+type Props = {
+	spec: Record<string, any>;
+};
+
+function ReactSwagger({ spec }: Props) {
+	// @ts-ignore - SwaggerUI is not typed
+	return <DynamicSwaggerUI spec={spec} />;
+}
+
+export default ReactSwagger;
