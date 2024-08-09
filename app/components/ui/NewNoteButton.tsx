d9649eeeea3d24c45e0ab4c2102714519b50208c
Santos Alarcón <santosalarcon86@gmail.com>
2024-08-09 19:08:43 +0200

    fix: sidebar open/close animation

--- app/components/ui/NewNoteButton.tsx
+++ app/components/ui/NewNoteButton.tsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/navigation";
 import sidebarStyles from "@/app/styles/sidebar.module.css";
 import NewNoteIcon from "../icons/NewNoteIcon";
 import { useNewNoteStore } from "@/app/store/newNoteStore";
-import { motion } from "framer-motion";
 
 const NewNoteButton = ({ title, expanded }: { title: string; expanded: boolean }) => {
 	const reset = useNewNoteStore.getState().reset;
@@ -15,26 +14,10 @@ const NewNoteButton = ({ title, expanded }: { title: string; expanded: boolean }
 		router.push("/notes/new");
 	};
 
-	const variants = {
-		expanded: {
-			opacity: 1,
-			display: "block"
-		},
-		collapsed: {
-			display: "none",
-			opacity: 0,
-			transition: {
-			    staggerChildren: 0.2,
-			    staggerDirection: -1,
-			    when: "afterChildren"
-			}
-		}
-	}
-
 	return (
-		<button data-title={title} className={sidebarStyles.sidebar__button} onClick={createNewNote} type="button">
+		<button data-title={title} className={expanded ? sidebarStyles.sidebar__button : sidebarStyles.sidebar__new__note__button__collapsed} onClick={createNewNote} type="button">
 			<NewNoteIcon />
-			<motion.span variants={variants} initial={expanded ? "collapsed" : "expanded"} exit={expanded ? "collapsed" : "expanded"} animate={expanded ? "expanded" : "collapsed"}>{title}</motion.span>
+			{expanded ? <span>{title}</span> : null}
 		</button>
 	);
 };
