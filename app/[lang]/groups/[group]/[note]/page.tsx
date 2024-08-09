14535dcdeb17618a22ac262827598c0ef93167a0
Santos Alarcón <santosalarcon86@gmail.com>
2024-08-09 20:18:35 +0200

    add: note header in pinned and group routes

--- app/[lang]/groups/[group]/[note]/page.tsx
+++ app/[lang]/groups/[group]/[note]/page.tsx
@@ -1,16 +1,23 @@
-import { getNoteById } from '@/app/utils/notes/getNoteById';
+import { getNoteById } from '@/app/utils/database/notes/getNoteById';
 import allNotesPageStyles from '@/app/styles/AllNotesPage.module.css'
 import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
 import styles from '@/app/styles/NotePage.module.css'
 import NoteList from '@/app/components/NoteList';
+import NoteHeader from '@/app/components/ui/NoteHeader';
+import rehypeHighlight from 'rehype-highlight';
+import remarkGfm from 'remark-gfm';
+import remarkHtml from 'remark-html';
 
-const GroupNotePage = ({ params: { group, note } }: { params: { group: string, note: string } }) => {
-	const foundNote = getNoteById(note);
+const GroupNotePage = async ({ params: { group, note, lang } }: { params: { group: string, note: string, lang: string } }) => {
+	const foundNote = await getNoteById(note);
 	return (
 		<main className={allNotesPageStyles.all__notes__page__container}>
 			<NoteList group={group} selected={note} />
-			<Markdown className={styles.note__page__markdown} remarkPlugins={[remarkGfm]}>{foundNote?.data}</Markdown>
+			<div className={allNotesPageStyles.all__notes__page__right}>
+			    <NoteHeader note={foundNote} lang={lang} />
+			    <Markdown className={styles.note__page__markdown} rehypePlugins={[rehypeHighlight]} remarkPlugins={[remarkGfm, remarkHtml]}>{foundNote?.data}</Markdown>			
+			</div>
+
 		</main>
 	)
 }
--- app/[lang]/notes/pinned/[note]/page.tsx
+++ app/[lang]/notes/pinned/[note]/page.tsx
@@ -1,18 +1,24 @@
 import NoteList from '@/app/components/NoteList';
+import NoteHeader from '@/app/components/ui/NoteHeader';
 import allNotesPageStyles from '@/app/styles/AllNotesPage.module.css';
 import styles from '@/app/styles/NotePage.module.css';
-import { getNoteById } from '@/app/utils/notes/getNoteById';
-import React from 'react'
+import { getNoteById } from '@/app/utils/database/notes/getNoteById';
 import Markdown from 'react-markdown';
+import rehypeHighlight from 'rehype-highlight';
 import remarkGfm from 'remark-gfm';
+import remarkHtml from 'remark-html';
 
-const PinnedNoteIdPage = ({params: {note}}: {params: {note: string}}) => {
-	const foundNote = getNoteById(note);
+const PinnedNoteIdPage = async ({params: {note, lang}}: {params: {note: string, lang: string}}) => {
+	const foundNote = await getNoteById(note);
 	return (
 		<main className={allNotesPageStyles.all__notes__page__container}>
 			<NoteList group="pinned" selected={note} />
-			<Markdown className={styles.note__page__markdown} remarkPlugins={[remarkGfm]}>{foundNote?.data}</Markdown>
+			<div className={allNotesPageStyles.all__notes__page__right}>
+			    <NoteHeader note={foundNote} lang={lang} />
+			    <Markdown className={styles.note__page__markdown} rehypePlugins={[rehypeHighlight]} remarkPlugins={[remarkGfm, remarkHtml]}>{foundNote?.data}</Markdown>			
+			</div>
 		</main>
+
 	)
 }
 
