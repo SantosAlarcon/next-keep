e85b9008bc53dd67f40bea513c0f4aa5b2ced93a
Santos Alarcón <santosalarcon86@gmail.com>
2024-08-11 21:36:07 +0200

    add: primereact dependency to the confirmation dialog

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -4,9 +4,9 @@ import "../styles/globals.css";
 import type { ReactNode } from "react";
 import i18NextConfig from "@/i18n.config";
 import { Toaster } from "sonner";
-import SidebarClient from "../components/SidebarClient";
 import { getAllData } from "../utils/getAllData";
 import dynamic from "next/dynamic";
+import "primereact/resources/themes/saga-blue/theme.css";
 
 const font = Lato({ subsets: ["latin"], weight: ["400", "700", "900"] });
 
@@ -14,7 +14,7 @@ export async function generateStaticParams() {
 	return i18NextConfig.i18n.locales.map((locale: string) => ({ locale }));
 }
 
-const SidebarClientNoSSR = dynamic(() => import('@/components/SidebarClient'), {ssr: false})
+const SidebarClientNoSSR = dynamic(() => import("@/components/SidebarClient"), { ssr: false });
 
 export const metadata: Metadata = {
 	title: {
@@ -40,9 +40,9 @@ export default async function RootLayout({
 	return (
 		<html lang={lang}>
 			<body className={font.className}>
-				<Toaster richColors position="bottom-center" theme="dark" />
-				<SidebarClientNoSSR params={{ lang: lang }} data={{ allNotes, allPinnedNotes, allGroups, allNoteAmounts }} />
-				{children}
+					<Toaster richColors position="bottom-center" theme="dark" />
+					<SidebarClientNoSSR params={{ lang: lang }} data={{ allNotes, allPinnedNotes, allGroups, allNoteAmounts }} />
+					{children}
 			</body>
 		</html>
 	);
--- app/components/ui/DeleteButton.tsx
+++ app/components/ui/DeleteButton.tsx
@@ -3,17 +3,24 @@
 import deleteButtonStyles from "@/styles/DeleteButton.module.css";
 import DeleteIcon from "../icons/DeleteIcon";
 import { deleteNote } from "@/app/utils/notes/deleteNote";
+import { confirmDialog, ConfirmDialog } from "primereact/confirmdialog";
+import { useState } from "react";
 
-function DeleteButton({ label, noteId, confirmString }: { label: string; noteId: string, confirmString: string }) {
+function DeleteButton({ label, noteId, confirmString }: { label: string; noteId: string; confirmString: string }) {
+	const [visible, setVisible] = useState<boolean>(false);
 	const handleClick = () => {
-        const answer = confirm(confirmString);
-
-        if (answer) {
-            deleteNote(noteId)
-        }
-    };
+        setVisible(true)
+		confirmDialog({
+			message: confirmString,
+			header: "Proceed with the deletion?",
+			icon: "pi pi-exclamation-triangle",
+			accept: () => deleteNote(noteId),
+			reject: () => setVisible(false),
+		});
+	};
 	return (
 		<button onClick={handleClick} type="button" data-title={label} className={deleteButtonStyles.delete__button__container}>
+			<ConfirmDialog visible={visible} onHide={() => setVisible(false)} />
 			<DeleteIcon width="18px" height="18px" />
 		</button>
 	);
