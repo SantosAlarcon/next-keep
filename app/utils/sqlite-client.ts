b00a75b61b6b05d95c422b067a35ab7f58b988ff
Santos Alarcón <santosalarcon86@gmail.com>
2024-08-21 21:01:48 +0200

    fix: Build errors Build errors

--- app/utils/sqlite-client.ts
+++ app/utils/sqlite-client.ts
@@ -1,5 +1,6 @@
-import sqlite3, { Database } from "sqlite3"
+import sqlite3, { type Database } from "sqlite3"
 
+// @ts-ignore
 export const db: Database = new sqlite3.Database(process.env.DATABASE_URL, sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
 	console.error("Fallo al conectar con la BD de SQLite.")
