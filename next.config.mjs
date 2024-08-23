bd51fc26f6e8db86ab0fbbf6a1229d4b565763c6
Santos Alarcón <santosalarcon86@gmail.com>
2024-08-23 13:19:22 +0200

    chore: Changed global checkbox style

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
-    transpilePackages: ["swagger-ui-react"],
+	pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+	transpilePackages: ["swagger-ui-react"],
+	optimizeFonts: true,
+	experimental: {
+	optimizePackageImports: ["primereact", "@uiw/react-markdown-editor", "@uiw/react-markdown-preview"],
+    }
 };
 
 export default nextConfig;
