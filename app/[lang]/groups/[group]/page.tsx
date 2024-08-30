9fcc452565592344ee0dcfab5ef2406119c18c66
Santos Alarcón <santosalarcon86@gmail.com>
2024-08-30 19:44:43 +0200

    style: added backdrop blur on dialog

--- app/[lang]/groups/[group]/page.tsx
+++ app/[lang]/groups/[group]/page.tsx
@@ -1,14 +1,11 @@
 import NoteList from "@/app/components/NoteList";
 
 const GroupPage = ({ params: { group } }: { params: { group: string } }) => {
-
 	return (
-        <>
-            <main>
-                <NoteList selected={"group"} group={group} />
-            </main>
-        </>
-	)
+		<main>
+			<NoteList selected={"group"} group={group} />
+		</main>
+	);
 };
 
 export default GroupPage;
