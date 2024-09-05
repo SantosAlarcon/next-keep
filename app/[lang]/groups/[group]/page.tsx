c8aaa6c8df013bf208ca9c6b890e107fe4fa9531
Santos Alarcón <santosalarcon86@gmail.com>
2024-09-05 20:10:57 +0200

    chore: added all note pages container style to group route

--- app/[lang]/groups/[group]/page.tsx
+++ app/[lang]/groups/[group]/page.tsx
@@ -1,9 +1,10 @@
 import FilterComponent from "@/app/components/FilterComponent";
 import NoteList from "@/app/components/NoteList";
+import allNotesPageStyles from "@/styles/AllNotesPage.module.css"
 
 const GroupPage = ({ params: { lang, group } }: { params: { lang: string; group: string } }) => {
 	return (
-		<main>
+		<main className={allNotesPageStyles.all__notes__page__container}>
 			<FilterComponent lang={lang} />
 			<NoteList selected={"group"} group={group} lang={lang} />
 		</main>
