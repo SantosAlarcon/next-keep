ed0619a31aaf6a4b5f951cec8bb806406611f891
Santos Alarcón <santosalarcon86@gmail.com>
2024-09-20 10:23:41 +0200

    fix?: create groups by using Appwrite API

--- app/utils/groups/createNewGroup.ts
+++ app/utils/groups/createNewGroup.ts
@@ -1,12 +1,22 @@
-import { appwriteProjectId } from "@/app/constants";
+import { appwriteAPIKey, appwriteProjectId, groupsEndpoint } from "@/app/constants";
+import { getSession } from "../getSession";
+import { ID } from "appwrite";
 
 export const createNewGroup = async (newGroup: string) => {
-	return await fetch(`${process.env.NEXT_PUBLIC_URL}/api/groups_appwrite`, {
+	const session = await getSession();
+	return await fetch(groupsEndpoint, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
-			"X-appwrite-project": appwriteProjectId,
+			"X-Appwrite-Project": appwriteProjectId,
+			"X-Appwrite-Key": appwriteAPIKey
 		},
-		body: JSON.stringify({ title: newGroup }),
+		body: JSON.stringify({
+			documentId: ID.unique(), 
+			data: {
+				title: newGroup,
+				userId: session.userId
+			}
+		}),
 	});
 };
