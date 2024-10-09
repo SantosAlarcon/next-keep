033690f05b0568d50ceae606b35bdc4420f2d733
Santos Alarcón <santosalarcon86@gmail.com>
2024-10-09 13:21:26 +0200

    removed pending from filter component

--- app/components/FilterComponent.tsx
+++ app/components/FilterComponent.tsx
@@ -8,18 +8,20 @@ import { dataStore } from "../store/dataStore";
 import { useTranslation } from "react-i18next";
 
 const FilterComponent = ({ lang }: { lang: string }) => {
-    const {t} = useTranslation("common", {lng: lang})
+	const { t } = useTranslation("common", { lng: lang })
 	// @ts-ignore
 	const setFilter = dataStore((state) => state.setFilter)
 	const [input, setInput] = useState<string>("");
-	const [debInput, pending] = useDebounce(input, 250);
+	const [debInput] = useDebounce(input, 250);
 
-	useEffect(() => { setFilter(debInput) }, [debInput]);
+	useEffect(() => {
+		setFilter(debInput)
+	}, [debInput]);
 
 	return (
 		<section className={`${FilterComponentStyles.filter__component__container} p-inputgroup`}>
 			<span className="p-inputgroup-addon">
-				<i className={pending ? "pi pi-spin pi-spinner-dotted" : "pi pi-filter"} />
+				<i className={"pi pi-filter"} />
 			</span>
 			{/* @ts-ignore */}
 			<InputText className={FilterComponentStyles.filter__component__input} placeholder={t("filter")} onInput={(e) => setInput(e.target.value)} />
